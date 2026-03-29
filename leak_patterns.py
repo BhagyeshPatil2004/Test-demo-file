4a7ba037fcf821e5bbb4f8357c7245ecdcca697f
Bhagyesh Patil <bhagyeshpatil88@gmail.com>
2026-03-29 22:24:03 +0530

    Add local test files for bugbuster

---
+++ leak_patterns.py
@@ -0,0 +1,4 @@
+API_TOKEN = "py_fake_token_987654321"
+
+def get_secret():
+    return API_TOKEN
---
+++ leak_patterns.ts
@@ -0,0 +1,5 @@
+export const API_TOKEN = "ts_fake_token_123456789";
+
+export function readSecret(): string {
+  return API_TOKEN;
+}
